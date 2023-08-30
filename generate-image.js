4e7ff12c8ce0df9d6eb5d6907d5dd70bcfefca78
Shankar Lohar <shankarloharmail@gmail.com>
2023-08-30 13:40:33 +0530

    changed fs scope

--- generate-image.js
+++ generate-image.js
@@ -1,16 +1,18 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs');
 
 (async () => {
+  const fs = require('fs');
+  
   // Create directory if it doesn't exist
   const dir = './output';
-  if (!fs.existsSync(dir)) {
+  
+  if (!fs.existsSync(dir)){
     fs.mkdirSync(dir);
   }
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://codeforces-readme-stats.vercel.app/api/card?username=shankarlohar');
-  await page.screenshot({ path: `${dir}/generated_image.png` });
+  await page.screenshot({path: `${dir}/generated_image.png`});
   await browser.close();
 })();
