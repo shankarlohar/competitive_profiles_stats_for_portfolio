92bbd0f132bc42a1a061dcef75090526a3e4e341
Shankar Lohar <shankarloharmail@gmail.com>
2023-08-30 13:38:15 +0530

    brought back old js

--- generate-image.js
+++ generate-image.js
@@ -1,16 +1,16 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
 (async () => {
-  try {
-    const dir = './output';
-    if (!fs.existsSync(dir)){
-        fs.mkdirSync(dir);
-    }
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://codeforces-readme-stats.vercel.app/api/card?username=shankarlohar');
-    await page.screenshot({ path: `${dir}/generated_image.png` });
-    await browser.close();
-  } catch (error) {
-    console.error("Error generating image:", error);
-    process.exit(1);  // Causes the GitHub Action to fail if any error occurs
+  // Create directory if it doesn't exist
+  const dir = './output';
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
   }
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://codeforces-readme-stats.vercel.app/api/card?username=shankarlohar');
+  await page.screenshot({ path: `${dir}/generated_image.png` });
+  await browser.close();
 })();
